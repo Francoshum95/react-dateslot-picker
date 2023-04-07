c1dddef27e8fe6188d921699b1cbf7176db7bf5c
franco <a17049322s@connect.polyu.hk>
2023-04-07 19:44:53 +0000

    deploy setting

--- site/storybook/vite.config.ts
+++ site/storybook/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@storybook/preview-web']
+      external: ['@storybook/preview-web', "@storybook/client-api"]
     }
   }
 })
